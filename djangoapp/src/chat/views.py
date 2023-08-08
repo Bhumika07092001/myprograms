760d9a3abc3bea55f209d9a53c879555ca2ddfcc
Bhumika07092001 <109783089+Bhumika07092001@users.noreply.github.com>
2023-08-08 17:11:33 +0530

    Update views.py

--- djangoapp/src/chat/views.py
+++ djangoapp/src/chat/views.py
@@ -1,11 +1,56 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .forms import SignupForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,get_object_or_404
 
-# Create your views here.
+from .models import Room, Message
+
+# @login_required
+def rooms(request):
+    all_rooms = Room.objects.all()
+
+    return render(request, 'rooms.html', {'rooms': all_rooms})
 
 
-def index(request):
-    return render(request,'index.html',{})
+def user_chat(request,room_slug):
+    room = get_object_or_404(Room, slug=room_slug)
+    # messages = Message.objects.filter(room=room).order_by('-timestamp')
 
-def room(request,room_name):
-    return render(request,'chatroom.html',
-                  {'room_name':room_name})
+    return render(request, 'user_chat.html',{'room': room})
+
+def get_messages(request):
+    messages = Message.objects.all().values('content')
+    return JsonResponse(list(messages), safe=False)
+
+@csrf_exempt
+def send_message(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        content = data.get('content')
+        if content:
+            Message.objects.create(content=content)
+            return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'error'}, status=400)
+
+
+def frontpage(request):
+    return render(request, 'front_page.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('frontpage')
+    else:
+        form = SignupForm()
+    
+    return render(request, 'signup.html', {'form': form})
